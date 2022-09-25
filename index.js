84a2dd19afbc0275c72c828940b9eacf2a09ab4a
Joakim Ekman <joakimekman91@gmail.com>
2022-09-26 00:42:46 +0200

    add properties with associated getter methods

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
 
+class FakePerson {
+    #fullName
+    #age
+    #email
+    #country
+    #gender
+    #password 
 
-console.log("Hello world!")
+    constructor () {
+        this.#gender = 'male'
+        this.#fullName = 'john doe'
+        this.#age = 44
+        this.#email = 'john.doe@example.com'
+        this.#country = 'Sweden'
+        this.#password = 'mysupersecretpassword123'
+    }
+
+    getGender () {
+        return this.#gender
+    }
+
+    getFullName () {
+        return this.#fullName
+    }
+
+    getAge () {
+        return this.#age
+    }
+
+    getEmail () {
+        return this.#email
+    }
+
+    getCountry () {
+        return this.#country
+    }
+
+    getPassword () {
+        return this.#password
+    }
+}
+
+const computer = new FakePerson()
+console.log("gender: ", computer.getGender())
+console.log("full name: ", computer.getFullName())
+console.log("age: ", computer.getAge())
+console.log("email: ", computer.getEmail())
+console.log("country: ", computer.getCountry())
+console.log("password: ", computer.getPassword())
