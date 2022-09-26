e6f23cf293cafd5c3911a3c52700da50194e0cc0
Joakim Ekman <joakimekman91@gmail.com>
2022-09-26 17:26:17 +0200

    first iteration of commenting

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { countries } from './countryList.js'
 import { letters, specialCharacters } from './characterList.js'
 
 
+/** Class representing a fake person. */
 class FakePerson {
     #fullName
     #age
@@ -11,6 +12,9 @@ class FakePerson {
     #gender
     #password 
 
+    /**
+     * Creates a fake person.
+     */
     constructor () {
         this.#gender = this.#generateGender()
         this.#fullName = this.#generateFullName()
@@ -20,6 +24,11 @@ class FakePerson {
         this.#password = this.#generatePassword()
     }
 
+    /**
+     * Create one or many fake persons at once.
+     * @param {number} num - The amount of fake persons to create.
+     * @returns {Array} - Array of fake persons.
+     */
     static createMany = (num) => {
         const people = []
 
@@ -31,18 +40,30 @@ class FakePerson {
         return people
     }
 
+    /**
+     * Generate a random gender.
+     * @returns {string} - The gender as in male or female.
+     */
      #generateGender = () => {
         const genders = ['male', 'female'] 
         const randomGender = genders[Math.floor(Math.random() * 2)]
         return randomGender
     }
 
+    /**
+     * Generate a random full name.
+     * @returns {string} - The full name including a first and last name.
+     */
     #generateFullName = () => {
         const firstName = this.#generateFirstName()
         const lastName = this.#generateLastName()
         return firstName + ' ' + lastName
     }
 
+    /**
+     * Generate a random first name based on the gender of the fake person.
+     * @returns {string} - A male or a female first name.
+     */
     #generateFirstName = () => {
         // catch if no gender
 
@@ -53,24 +74,44 @@ class FakePerson {
         }
     }
 
+    /**
+     * Generate a random last name based on the gender of the fake person.
+     * @returns {string} - A last name.
+     */
     #generateLastName = () => {
         return lastNames[Math.floor(Math.random() * lastNames.length)]
     }
 
+    /**
+     * Generate a random age.
+     * @returns {number} - An age.
+     */
     #generateAge = () => {
         return Math.floor(Math.random() * 90 + 18)
     }
 
-
+    /**
+     * Generate a random email based on the first and last name of the fake person.
+     * @returns {string} - An email.
+     */
     #generateEmail = () => {
         const email = this.getFirstName() + '.' + this.getLastName() + '@example.com'
         return email.toLowerCase()
     }
 
+    /**
+     * Generate a random country.
+     * @returns {string} - A country.
+     */
     #generateCountry = () => {
         return countries[Math.floor(Math.random() * (countries.length))]
     }
 
+    /**
+     * Generate a random and unique 12 word password including special characters, digits, as well as upper and lower
+     * letters.
+     * @returns {string} - A password.
+     */
     #generatePassword = () => {
         let password = ''
 
@@ -85,72 +126,136 @@ class FakePerson {
         return password
     }
 
+    /**
+     * 
+     * @returns {string} - A gender. 
+     */
     getGender () {
         return this.#gender
     }
 
+    /**
+     * 
+     * @returns {string} - A full name.
+     */
     getFullName () {
         return this.#fullName
     }
 
+    /**
+     * 
+     * @returns {string} - A first name.
+     */
     getFirstName = () => {
         return this.#fullName.split(' ')[0]
     }
 
+    /**
+     * 
+     * @returns {string} - A last name. 
+     */
     getLastName = () => {
         return this.#fullName.split(' ')[1]
     }
 
+    /**
+     * 
+     * @returns {number} - An age.
+     */
     getAge () {
         return this.#age
     }
 
+    /**
+     * 
+     * @returns {string} - An email.
+     */
     getEmail () {
         return this.#email
     }
 
+    /**
+     * 
+     * @returns {string} - A country.
+     */
     getCountry () {
         return this.#country
     }
 
+    /**
+     * 
+     * @returns {string} - A password.
+     */
     getPassword () {
         return this.#password
     }
 
+    /**
+     * 
+     * @returns {string} - An description.
+     */
     getDescription = () => {
         return `Hello, my name is ${this.#fullName} and I am ${this.#age} years old. I live in ${this.#country} and you can contact me at ${this.#email}.`
     }
 
-    guess = (options) => {
+    /**
+     * Randomly selects an option from an array of options.
+     * @param {Array} options - An array of options.
+     * @returns {string|number} - The randomly selected value.
+     */
+    randomSelection = (options) => {
         const selection = options[Math.floor(Math.random() * (options.length))]
         return selection
     }
 
-    rollDice = (sides, numOfDices) => {
+    /**
+     * Roll one or many dice with X faces.
+     * @param {*} sides - The number of faces the dice has.
+     * @param {*} numOfDices - The number of dice that should be rolled.
+     * @returns {Array} - Array containing the result from the dice roll(s).
+     */
+    rollDice = (faces, numOfDices) => {
+        // error handling => can't be null or minus.
         const rolls = []
 
         for (let i = 0; i < numOfDices; i++) {
-             const roll = Math.floor((Math.random() * sides) + 1)
+             const roll = Math.floor((Math.random() * faces) + 1)
              rolls.push(roll)
         }
 
         return rolls
     }
 
+    /**
+     * Play the rock paper scissor game.
+     * @returns {string} - Rock, paper or scissor.
+     */
     playRockPaperScissor = () => {
         const options = ['rock', 'paper', 'scissor']
         return options[Math.floor(Math.random() * (options.length))]
     }
 
+    /**
+     * Determines whether the fake person will answer correctly based on its skill level. For example,
+     * a beginner is 6/10 likely to answer correctly while an expert is 8/10 likely to answer correctly.
+     * 
+     * @param {string} skillLevel - Determines how likely the fake person is to answer correctly.
+     * @returns {boolean} - Whether the fake person answered correctly or not.
+     */
     #answeredCorrectly = (skillLevel) => {
-        // Map skill level to an integer.
         const skillLevels = { expert: 8, average: 7, beginner: 6 }
-
         const randNum = Math.floor((Math.random() * 10) + 1)
-        return randNum <= skillLevel
+        return randNum <= skillLevels[skillLevel]
     }
 
+    /**
+     * Attempt to answer a quiz question correctly based on the skill level.
+     * @param {object} question - An object containing the options as an array and the correct answer as a string.
+     * @param {string} skillLevel - Determines how likely the fake person is to answer correctly.
+     * @returns {string} - The answer given by the fake person.
+     */
     answerQuizQuestion = (question, skillLevel = 'average') => {
+        // Error handling; unknown skillLevel.
         const { options, correctAnswer } = question
 
         if (this.#answeredCorrectly(skillLevel)) {
@@ -186,6 +291,6 @@ const question = {
 }
 console.log("quiz answer: ", computer.answerQuizQuestion(question, 'beginner'))
 
-const people = FakePerson.createMany(10)
-console.log(people)
-console.log(people[0])
+// const people = FakePerson.createMany(10)
+// console.log(people)
+// console.log(people[0])
